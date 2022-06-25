c5ec9d1f2cd6c061fd4165d9d689c647bd2d2002
Chibuike Nnorom <nnoromchibuikepromise@gmail.com>
2022-06-25 12:05:09 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let myName = 'Chibuike Nnorom'
+let myHeight = '6.1ft'
+let myCountry = 'Nigeria'
+
+document.write('Name: ' + myName + '<br>');
+document.write('Height:  ' + myHeight + '<br>');
+document.write('Country: ' + myCountry);
